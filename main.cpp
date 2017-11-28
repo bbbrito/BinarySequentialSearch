c3ef5c39060ed61c65abf635e0d92a6b8c5b711a
Brynner Brito <brynner_engc@hotmail.com>
2017-11-28 02:14:37 -0300

    Criar funções de Sequential e Binary Search, e calcular a média de passos de cada função

--- main.cpp
+++ main.cpp
@@ -5,52 +5,110 @@ using std::cout;
 using std::cin;
 #include <vector>
 using std::vector;
-#include <math.h>
 
 bool trocar ( int &, int & );
 bool trocar ( double &, double & );
 void outputVector( const vector< double > & );
 void bubbleSort( vector< double > & );
 void fillVector( vector< double > & );
-void imprimeVetor ( vector< double > & );
-
-
+void imprimeVetor( vector< double > & );
+void funcArray( int [], vector< double > & );
+int sequentialSearch( int &, int [], int & );
+int binarySearch( int &, vector< double > & );
 
 int main( )
 {
     int numberSearch = 0;
+    int size = 0;
     srand( ( int ) time ( 0 ) );
     vector< double > vetorFinal;
+	const int NITER = 5;
+	int numberStep1 = 0;
+	int numberStep2 = 0;
+	double mediaSequential = 0.0;
+	double mediaBinary = 0.0;
+    	
+	int totalStepsSequential = 0;
+	int totalStepsBinary = 0;
+    for (int i = 0; i <= NITER - 1; i++)
+    {
+	    fillVector( vetorFinal );
+	    imprimeVetor( vetorFinal );
+	    
+	    bubbleSort( vetorFinal );
+	    imprimeVetor( vetorFinal );
+	    
+	    int myArray[ vetorFinal.size() ];	    
+	    funcArray( myArray, vetorFinal );
 
+		//Sequential Search
+		numberSearch = myArray[ rand() % ( vetorFinal.size( ) - 1 ) ];//Intervalo de acordo com o tamanho do vetor fornecido pelo usuario.
+		size = 	vetorFinal.size();
+        numberStep1 = sequentialSearch( numberSearch, myArray, size );
+        
+        //Binary Search
+    	numberStep2 = binarySearch( numberSearch, vetorFinal );    	
+    	totalStepsSequential += numberStep1;    	
+        totalStepsBinary += numberStep2;
+	}
+    mediaSequential = (double)totalStepsSequential / (double)NITER; 
+	mediaBinary = (double)totalStepsBinary / (double)NITER;
+	
+    cout << "Media de passos da Sequential Search: " << mediaSequential << '\n';
+    cout << "Media de passos da Binary Search: " << mediaBinary << '\n';
+ 
+    system ("pause"); 
+    return 0;
 
-    fillVector( vetorFinal );
-    imprimeVetor( vetorFinal );
+}
+
+int binarySearch( int & numberSearch, vector< double > & vetor2 )
+{
+	int low = 0;
+    int mid = 0;
+    int high = 0;
+    int middle = 0;
+    int step2 = 1;
     
-    bubbleSort( vetorFinal );
-    imprimeVetor( vetorFinal );
+    do
+    {
+        mid = vetor2[ ( vetor2.size( ) - 1 ) / 2 ];         
+        middle = ( vetor2.size( ) - 1 ) / 2;
+        if( numberSearch < mid )
+        {
+            vetor2.erase( vetor2.begin( )+middle, vetor2.end( ) );
+            step2++;
+        }          
+        else if ( numberSearch > mid && vetor2.size( ) > 2 )
+        {               
+            vetor2.erase( vetor2.begin( ), vetor2.begin( )+middle );
+            step2++;
+        }
+        else if ( numberSearch > mid && vetor2.size( ) <= 2 )
+        {               
+            vetor2.erase( vetor2.begin( ) );
+            step2++;
+        }
+    } while( numberSearch != mid );       
     
-    int myArray[ vetorFinal.size() ];
+	cout << "Numero encontrado na Binary Search: " << mid << '\n';
+    cout << "Numero de passos na Binary Search: " << step2 << "\n\n";
     
-    for(int i = 0; i < vetorFinal.size( ); i++)
-        myArray[ i ] = vetorFinal[ i ];
-       
-    cout << "Os valores do Array sao: " << '\n';
-    for ( int i = 0; i < vetorFinal.size( ); i++ )
-        cout << myArray[ i ] << '\n';       
-    cout << '\n';
+    vetor2.erase( vetor2.begin( ), vetor2.end( ) );
 
+    return step2;
+}
 
-
-    //Sequencial Search 
+int sequentialSearch( int & numberSearch, int myArray[ ], int & sizeMyArray )
+{
     int step1 = 1;
-    numberSearch = myArray[ vetorFinal.size( ) - 3 ];
-    cout << "O numero a ser encontrado eh: " << numberSearch << '\n';
-    for( int i = 0; i < vetorFinal.size( ); i++ )
+    cout << "O numero a ser encontrado eh: " << numberSearch << "\n\n";
+    for( int i = 0; i < sizeMyArray; i++ )
     {
         if( numberSearch == myArray[ i ] )
         {
-            cout << "Numero encontrado na Sequencial Search: " << myArray[ i ] << '\n';
-            cout << "Numero de passos na Sequencial Search: " << step1 << '\n';
+            cout << "Numero encontrado na Sequential Search: " << myArray[ i ] << '\n';
+            cout << "Numero de passos na Sequential Search: " << step1 << "\n\n";
             break;
         }
         else
@@ -58,50 +116,8 @@ int main( )
             step1++;
         }
     }
-
-    //Binary Search
-    numberSearch = vetorFinal[ vetorFinal.size( ) - 3 ];
-    int low = 0;
-    int mid = vetorFinal[ ( vetorFinal.size( ) - 1 ) / 2 ];
-    int high = 0;
-    int middle = 0;
-    int step2 = 1;
-    
-    while( numberSearch != mid )
-    {
-			low = vetorFinal[ 0 ];
-            mid = vetorFinal[ ( vetorFinal.size( ) - 1 ) / 2 ];
-            high = vetorFinal[ vetorFinal.size( ) - 1 ];
-            
-            cout << "Primeiro elemento: " << low << '\n';
-            cout << "Elemento central: " << mid << '\n';
-            cout << "Elemento final: " << high << '\n';
-				           
-            middle = ( vetorFinal.size( ) - 1 ) / 2;
-
-            if( numberSearch < mid )
-            {
-                vetorFinal.erase( vetorFinal.begin( )+middle, vetorFinal.end( ) );
-                step2++;
-            }          
-            else if ( numberSearch > mid )
-            {               
-                vetorFinal.erase( vetorFinal.begin( ), vetorFinal.begin( )+middle );
-                step2++;
-            }
-
-    }
-    if ( numberSearch == mid )
-        {               
-            cout << "Numero encontrado na Binary Search: " << mid << '\n';
-    		cout << "Numero de passos na Binary Search: " << step2 << '\n';
-        }
-  
-    system ("pause"); 
-    return 0;
-
+    return step1;
 }
-
 
 void bubbleSort( vector< double > & vetor)
 {
@@ -117,20 +133,21 @@ void bubbleSort( vector< double > & vetor)
         }
     }
         
-    cout << "Total de trocas: " << count << '\n';
+    cout << "Total de trocas: " << count << "\n\n";
     
 
 }
 
 void fillVector( vector< double > & vetor )
 {
+	cout << "............................................" << '\n';
 	int count = 0;
     int size = 0;
     cout << "Digite um inteiro para o tamanho do vetor: " << '\n';
     cin >> size;
     while( count < size )
     {        
-        vetor.push_back( 100.0 * double(1 + rand() % 30) / 30.0 );      
+        vetor.push_back( 100.0 * (1 + rand() % 30) / 30.0 );      
         count++;        
     } 
 }
@@ -170,5 +187,18 @@ void imprimeVetor( vector< double > & vetor )
 {
     cout << "Os valores do Vetor sao: " << '\n';
     for (int i = 0; i < vetor.size( ); i++)
-        cout << vetor[ i ] << '\n';
+        cout << vetor[ i ] << '\t';
+    cout << "\n\n";
 }
+
+void funcArray(int array[], vector< double > & vetor1)
+{
+    for(int i = 0; i < vetor1.size( ); i++)
+        array[ i ] = vetor1[ i ];
+       
+    cout << "Os valores do Array sao: " << '\n';
+    for ( int i = 0; i < vetor1.size( ); i++ )
+        cout << array[ i ] << '\t';       
+    cout << "\n\n";
+}
+
